70f2ed7eba20c54071f11085c4c65a8d652c5670
ankitashukla <ankishukla07@gmail.com>
2014-12-22 20:48:51 +0530

    Added instructions to create the db

--- sql/spelling_dictionary.sql
+++ sql/spelling_dictionary.sql
@@ -1,3 +1,12 @@
+-- Steps for creating a new database spelling_dictionary:
+-- mysql> CREATE DATABASE spelling_dictionary;
+-- mysql> USE spelling_dictionary;
+-- In the next line replace "USER" with the value of $wgDBuser from your LocalSettings.php,
+-- and replace localhost with your hostname if needed.
+-- mysql> GRANT ALL ON spelling_dictionary.* to 'USER'@'localhost';
+-- mysql> SOURCE spelling_dictionary.sql
+
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
