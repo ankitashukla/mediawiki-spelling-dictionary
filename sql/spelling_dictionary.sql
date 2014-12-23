c11e83d02ecc097650eda0c811ed91c24110b54d
ankitashukla <ankishukla07@gmail.com>
2014-12-23 11:26:43 +0530

    Fixed the json files
    -Removed comma from the end of lines in the localisation files
    -Fixed indentation of files
    -Converted indentation from spaces to tabs

--- sql/spelling_dictionary.sql
+++ sql/spelling_dictionary.sql
@@ -15,8 +15,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `word_list`
 
 CREATE TABLE IF NOT EXISTS `spell_dict_word_list` (
-  `words` varchar(50) NOT NULL,
-  `language` varchar(20) NOT NULL COMMENT 'Language to which the word belongs',
-  `timestamp` double NOT NULL,
-  `user` varchar(40) NOT NULL COMMENT 'Name of the user who submitted the word'
+	`words` varchar(50) NOT NULL,
+	`language` varchar(20) NOT NULL COMMENT 'Language to which the word belongs',
+	`timestamp` double NOT NULL,
+	`user` varchar(40) NOT NULL COMMENT 'Name of the user who submitted the word'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
