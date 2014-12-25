d59dcbf8c632f546793d8a9b7d69195474a0f230
ankitashukla <ankishukla07@gmail.com>
2014-12-25 23:21:37 +0530

    Changed the db schema as per db coding conventions

--- sql/spelling_dictionary.sql
+++ sql/spelling_dictionary.sql
@@ -14,9 +14,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 -- Table structure for table `word_list`
 
-CREATE TABLE IF NOT EXISTS `spell_dict_word_list` (
-	`words` varchar(50) NOT NULL,
-	`language` varchar(20) NOT NULL COMMENT 'Language to which the word belongs',
-	`timestamp` double NOT NULL,
-	`user` varchar(40) NOT NULL COMMENT 'Name of the user who submitted the word'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS /*_*/`spell_dict_word_list` (
+	`sd_word` varchar(50) NOT NULL,
+	`sd_language` varchar(20) NOT NULL COMMENT 'Language to which the word belongs',
+	`sd_timestamp` varbinary(14) NOT NULL,
+	`sd_user` varchar(40) NOT NULL COMMENT 'Name of the user who submitted the word'
+) /*$wgDBTableOptions*/ 
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
