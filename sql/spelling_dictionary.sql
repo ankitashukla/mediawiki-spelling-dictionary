f7b0493146e41dde2424730e96e8eb14348b1b73
ankitashukla <ankishukla07@gmail.com>
2014-12-30 23:40:48 +0530

    Added a check if table exists in database or not

--- sql/spelling_dictionary.sql
+++ sql/spelling_dictionary.sql
@@ -1,6 +1,6 @@
 -- Table structure for table `spell_dict_word_list`
 
-CREATE TABLE /*_*/spell_dict_word_list (
+CREATE TABLE IF NOT EXISTS /*_*/spell_dict_word_list (
 	sd_word varchar(50) NOT NULL,
 	sd_language varchar(20) NOT NULL COMMENT 'Language to which the word belongs',
 	sd_timestamp varbinary(14) NOT NULL,
