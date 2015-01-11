86886a4f440e491046c183eb02957dceda2c28e4
ankitashukla <ankishukla07@gmail.com>
2015-01-11 17:27:28 +0530

    Wrote code to display all words stored in database

--- specials/SpecialViewAll.php
+++ specials/SpecialViewAll.php
@@ -8,7 +8,20 @@ class SpecialViewAll extends SpecialPage {
 
 	public function execute( $sub ) {
 		$out = $this->getOutput();
-		$out->setPageTitle( $this->msg( 'title-special' ) );
+		$out->setPageTitle( $this->msg( 'title-view-all' ) );
 		$out->addWikiMsg( 'intro-paragraph' );
+
+		global $wgSpellingDictionaryDatabase;
+		$dbr = wfGetDB( DB_SLAVE, array(), $wgSpellingDictionaryDatabase );
+		$rows = $dbr->select(
+			'spell_dict_word_list',
+			'*',
+			1,
+			__METHOD__
+		);
+		$result = array();
+		foreach ( $rows as $row ) {
+			$out->addHTML ( $row->sd_word . " of language " . $row->sd_language . "<br>" );
+		}
 	}
 }
