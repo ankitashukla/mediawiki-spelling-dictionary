09ee1a08f7f94ea31f846ddd784740d43ffc041b
ankitashukla <ankishukla07@gmail.com>
2015-01-21 23:21:03 +0530

    Added section for import/export of word list

--- specials/SpecialSpellingDictionaryAdmin.php
+++ specials/SpecialSpellingDictionaryAdmin.php
@@ -17,13 +17,23 @@ class SpecialSpellingDictionaryAdmin extends SpecialPage {
 
 	public function initializeTree() {
 		$tree = new SDTree();
+
+		// SECTION: browse spellings
 		$browse_edit = new SDSection( $this->msg( 'sd-admin-browse-edit' )->text() );
+		// Item: View All
 		$viewall = SpecialPage::getTitleFor( 'ViewAll' );
-		$browse_edit->addItem( SDItem::showPage($viewall, $this->msg( 'sd-admin-viewall' )->text() ));
+		$browse_edit->addItem( SDItem::showPage( $viewall, $this->msg( 'sd-admin-viewall' )->text() ));
+		// Item: View By Language
 		$viewByLang = SpecialPage::getTitleFor( 'ViewByLanguage' );
-		$browse_edit->addItem( SDItem::showPage($viewByLang, $this->msg( 'sd-admin-view-by-lang' )->text() ));
+		$browse_edit->addItem( SDItem::showPage( $viewByLang, $this->msg( 'sd-admin-view-by-lang' )->text() ));
 
 		$tree->addSection( $browse_edit );
+
+		// SECTION: import and export
+		$import_export = new SDSection( $this->msg( 'sd-admin-importexport' )->text() );
+
+		$tree->addSection( $import_export );
+
 		return $tree;
 	}
 
